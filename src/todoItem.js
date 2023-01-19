5725496290c1bcf9ed1371337fefb276aa545b8c
JaimePerales <jaimeperales123@gmail.com>
2023-01-19 10:00:36 -0600

    create todoItem

---
+++ src/todoItem.js
@@ -0,0 +1,23 @@
+// At a minimum they should have a title, description, 
+// dueDate and priority. You might also want to 
+// include notes or even a checklist
+import { format } from 'date-fns'
+
+
+class TodoItem {
+
+    title = 'Task Title';
+
+    description = 'Task Description';
+
+    dueDate = format(new Date(), 'MM/dd/yyyy');
+
+    priority = 'HIGH';
+
+    notes = '';
+
+
+}
+
+
+export default TodoItem;
